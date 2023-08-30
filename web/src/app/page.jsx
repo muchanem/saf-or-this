2beab32450c6a014cdfb23877c01ebbd67c20cfe
Mark Muchane <mark@pztron.io>
2023-08-30 17:28:55 -0500

    this or saf MVP

---
+++ web/src/app/page.jsx
@@ -0,0 +1,54 @@
+'use client' 
+import React from "react"
+
+export default function Home() {
+    const [things, setThings] = React.useState([])
+    const [currentThing,setCurrent] = React.useState({})
+    const [loaded, setLoad] = React.useState("false")
+
+    React.useEffect(() => {
+        fetch("http://localhost:8000/things")
+            .then(response => {
+                return response.json()
+            })
+            .then(data => {
+                setThings(data["data"])
+                setCurrent(data["data"][0]) 
+                setLoad("loaded")
+            })
+    }, []);
+    const vote = (win) => {
+        if (win) {
+            fetch("http://localhost:8000/vote/?" + new URLSearchParams({"thingid": currentThing["uuid"]}))
+        } else {
+            fetch("http://localhost:8000/appeared/?" + new URLSearchParams({"thingid": currentThing["uuid"]}))
+        }
+        setCurrent(things[1])
+        setThings(things.slice(1))
+        if (things.length < 3) {
+            fetch("http://localhost:8000/things")
+            .then(response => {
+                return response.json()
+            })
+            .then(data => {
+                setThings(things.concat(data["data"]))
+            })
+        }
+    }
+    switch (loaded) {
+        case "false": {
+            return (
+                <p>That dam saf.</p>
+            )
+        }
+        case "loaded": {
+            return (
+            <main className="flex flex-col h-screen my-auto mx-auto space-y-4 font-mono text-white text-sm font-bold leading-6 max-w-xs justify-center">
+                <div onClick={() => vote(false)} className="select-none p-4 rounded-lg flex items-center justify-center bg-sky-500 shadow-lg">Saf</div>
+                <div onClick={() => vote(true)} className="select-none p-4 rounded-lg flex items-center justify-center bg-teal-500 shadow-lg">{currentThing["name"]}</div>
+                <a href="results"><div className="select-none p-4 rounded-lg flex items-center justify-center border-2 border-indigo-500 shadow-lg">Results</div></a>
+            </main>
+            )
+        }
+    }
+}
---
+++ web/src/app/results/page.jsx
@@ -0,0 +1,51 @@
+'use client'
+import React from "react"
+
+export default function Results() {
+    const [results,setResults] = React.useState([])
+    const [loaded, setLoad] = React.useState(false)
+    const safRef = React.useRef(null)
+    const scrollToSaf = () => safRef.current.scrollIntoView();
+    React.useEffect(() => {
+        fetch("http://localhost:8000/results")
+        .then(response => {
+            return response.json()
+        })
+        .then(data => {
+            setResults(data["data"])
+            setLoad(true)
+        })
+    }, [])
+    return(
+    <div>
+    { loaded ? ( 
+        <div className="flex flex-col h-screen w-1/4 py-auto mx-auto space-y-4 font-mono text-white text-sm font-bold leading-6 max-w-xs justify-center">
+            <div className="flex flex-col h-[50vh] overflow-y-auto">
+                {
+                    results.map((thing,index) => {
+                        if (thing["name"] == "Saf") {
+                            return (
+                            <div ref={safRef} key={index} className="select-none p-4 rounded-lg flex items-center justify-center shadow-lg border-2 border-teal-500">
+                                <div className="flex flex-grow justify-start">{index+1}.</div>
+                                <div className="flex flex-grow justify-center">{thing["name"]}</div>
+                                <div className="flex flex-grow justify-end">{thing["win_p"]}%</div>
+                            </div>)
+                        } else {
+                            return (
+                                <div key={index} className="select-none p-4 rounded-lg flex items-center justify-center shadow-lg">
+                                    <div className="flex flex-grow justify-start">{index+1}.</div>
+                                    <div className="flex flex-grow justify-center">{thing["name"]}</div>
+                                    <div className="flex flex-grow justify-end">{thing["win_p"]}%</div>
+                                </div>
+                            )
+                        }
+                    })
+                }
+            </div>
+                <div onClick={scrollToSaf} className="select-none p-4 rounded-lg flex items-center justify-center bg-sky-500 shadow-lg">See Saf!</div>
+                <a href="/"><div className="select-none p-4 rounded-lg flex items-center justify-center border-2 border-indigo-500 shadow-lg">Vote</div></a>
+        </div>
+        ) : (<p>That dam saf.</p>)
+    } </div>
+    )
+}
